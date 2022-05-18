3410251f41e9044a998899aac02b91a80572d7e7
TimVerbecque <r0788499@student.thomasmore.be>
2022-05-18 20:16:38 +0200

    servers table migration

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use App\Models\Role;
+use App\Models\Server;
 use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
@@ -37,5 +38,29 @@ class UserSeeder extends Seeder
         ]);
         $user->roles()->attach($admin);
         $user->roles()->attach($guest);
+        
+        $server = Server::create([
+            'user_id' => 1,
+            'server_ip' => '172.26.5.10',
+            'sftp_username' => 'slag',
+            'sftp_password' => Hash::make('slagen'),
+            'sftp_host' => '172.26.5.10',
+            'sftp_port' => 22,
+            'php_host' => '172.26.5.10',
+            'php_username' => 'phpslag',
+            'php_password' => Hash::make('phpslagen'),
+        ]);
+
+        $server = Server::create([
+            'user_id' => 2,
+            'server_ip' => '172.26.5.10',
+            'sftp_username' => 'pallettekop',
+            'sftp_password' => Hash::make('voelpallettekop'),
+            'sftp_host' => '172.26.5.10',
+            'sftp_port' => 22,
+            'php_host' => '172.26.5.10',
+            'php_username' => 'phppallettekop',
+            'php_password' => Hash::make('phpvoelpallettekop'),
+        ]);
     }
 }
