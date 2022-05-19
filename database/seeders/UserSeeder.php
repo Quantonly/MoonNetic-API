7bed4936a2f1ac2e4e2ea1460d5875457cee8c93
Cody Volz <quantonly.dev@gmail.com>
2022-05-19 08:56:42 +0200

    Model import

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
-use App\Models\Server;
+use App\Models\Website;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
@@ -27,7 +27,7 @@ class UserSeeder extends Seeder
             'email' => 'test.test@test.com',
             'password' => Hash::make('test1234'),
         ]);
-        $server = Website::create([
+        $website = Website::create([
             'user_id' => 1,
             'sub_domain' => 'codyvolz',
             'server_ip' => '172.26.5.10',
@@ -41,7 +41,7 @@ class UserSeeder extends Seeder
             'php_password' => Str::random(10),
             'php_version' => '7.4'
         ]);
-        $server = Website::create([
+        $website = Website::create([
             'user_id' => 2,
             'sub_domain' => 'testaccount',
             'server_ip' => '172.26.5.10',
