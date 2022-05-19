70b590d62127e72a8a12da37ffa41a21f69e8a98
Cody Volz <quantonly.dev@gmail.com>
2022-05-19 08:58:45 +0200

    Name fix

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
-use App\Models\Website;
+use App\Models\WebsiteUser;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
